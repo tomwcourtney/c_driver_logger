e563b5fbea4ece4de793cf6e7761429d099874ff
RS_Lappy\Pat <patrick.kingwill@roboticsystems.com.au>
2023-07-31 06:35:43 +1000

    Added Test Harness

---
+++ test-harness/tests/MyFirstTest.cpp
@@ -0,0 +1,28 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+	/*
+	 * Add your c-only include files here
+	 */
+}
+
+TEST_GROUP(MyCode)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(MyCode, test1)
+{
+    /*
+     * Instantiate your class, or call the function, you want to test.
+     * Then delete this comment
+     */
+    FAIL("You Failed")
+}
