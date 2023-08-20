cd70f982240d054d4053b93b4f5bbf1baa2181b8
LAPTOP\xolop <thomas.courtney@roboticsystems.com.au>
2023-08-21 06:53:29 +1000

    ignore gen docs

--- docs/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['logger_5fdestination_5ft_13',['logger_destination_t',['../structlogger__destination__t.html',1,'']]]
-];
--- docs/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['write_5ffunction_24',['write_function',['../logger_8h.html#a5e48e71c47e99dd78598f39df2f50823',1,'logger.h']]]
-];
